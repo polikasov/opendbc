11712ed0641e624ac2c1fabc7d5fa512e005fbb8
Anton <61949281+polikasov@users.noreply.github.com>
2025-04-25 09:58:01 +0400

    enable alphalong for subaru preglobal

--- opendbc/car/subaru/interface.py
+++ opendbc/car/subaru/interface.py
@@ -86,8 +86,8 @@ class CarInterface(CarInterfaceBase):
     else:
       raise ValueError(f"unknown car: {candidate}")
 
-    ret.alphaLongitudinalAvailable = not (ret.flags & (SubaruFlags.GLOBAL_GEN2 | SubaruFlags.PREGLOBAL |
-                                                       SubaruFlags.LKAS_ANGLE | SubaruFlags.HYBRID))
+    ret.alphaLongitudinalAvailable = not (ret.flags & (SubaruFlags.GLOBAL_GEN2 | SubaruFlags.LKAS_ANGLE |
+                                                       SubaruFlags.HYBRID))
     ret.openpilotLongitudinalControl = alpha_long and ret.alphaLongitudinalAvailable
 
     if ret.flags & SubaruFlags.GLOBAL_GEN2 and ret.openpilotLongitudinalControl:
